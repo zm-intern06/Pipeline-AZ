62c504abac683d2f5245a0427da34fbfd235f8df
zm-intern06 <zm-intern06@zmwarelab.com>
2021-05-02 17:18:47 -0400

    save changes

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "myfirstcontainer"
+  name                     = var.registry_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Premium"
