071a94762be8e314d4579f7f6ebcc987d712b8d6
zm-intern06 <zm-intern06@zmwarelab.com>
2021-05-02 16:54:25 -0400

    true change

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "contain_dock"
+  name                     = "myfirstcontainer"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Premium"
